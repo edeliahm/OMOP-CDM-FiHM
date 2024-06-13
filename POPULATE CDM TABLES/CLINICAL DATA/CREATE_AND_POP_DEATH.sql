f5ad9231ef04ecba45fe51da04a31cdd2c6f4a1a
Emiliano DElia Cerchi <144276034+edeliahm@users.noreply.github.com>
2024-06-13 15:28:20 +0200

    Update CREATE_AND_POP_DEATH.sql

--- POPULATE CDM TABLES/CLINICAL DATA/CREATE_AND_POP_DEATH.sql
+++ POPULATE CDM TABLES/CLINICAL DATA/CREATE_AND_POP_DEATH.sql
@@ -1,19 +1,3 @@
-/*
-IF OBJECT_ID('bbdd_emilianodelia.dbo.DEATH', 'U') IS NOT NULL
-BEGIN
-	DROP TABLE bbdd_emilianodelia.dbo.DEATH;
-END;
-CREATE TABLE bbdd_emilianodelia.dbo.DEATH (
-    person_id INT PRIMARY KEY,
-    death_date DATE,
-    death_datetime DATETIME,
-    death_type_concept_id INT,
-    cause_concept_id INT,
-    cause_source_value VARCHAR(50),
-    cause_source_concept_id INT
-);
-*/
-
 /*  ******************************************** */
 /*  ******************************************** */
 /*  ******************************************** */
@@ -22,17 +6,16 @@ SET DATEFORMAT 'YMD'
 
 TRUNCATE TABLE BBDD_EMILIANODELIA.DBO.DEATH
 
-INSERT INTO BBDD_EmilianoDelia.dbo.death(person_id, 
-										 death_date, 
-										 death_datetime, 
-										 death_type_concept_id, 
-										 cause_concept_id, 
-										 cause_source_value,
-										 cause_source_concept_id
-										 ) 
+INSERT INTO BBDD_EmilianoDelia.dbo.death(
+	 person_id, 
+	 death_date, 
+	 death_datetime, 
+	 death_type_concept_id, 
+	 cause_concept_id, 
+	 cause_source_value,
+	 cause_source_concept_id) 
 
 SELECT
-
 	i.IdPaciente as person_id, 
 	convert(date, i.Fechaalta) as death_date, 
 	CONVERT(varchar, CONVERT(date, i.fechaalta)) + ' ' + i.horaalta + ':00' as death_datetime, 
@@ -46,6 +29,5 @@ JOIN [Hosma-Doctor].DBO.parametros pp on pp.CodParametro = i.MotivoAlta and Grup
 JOIN [Hosma-Doctor].DBO.Organizacion o on o.IdHospital = i.IdHospital
 
 WHERE 
-	datepart(year, i.FechaIngreso) between '2019' and '2023' and -- range of dates just for testing 
-	o.NombreCorto like 'madrid' and  -- only madrid just for testing 
+	datepart(year, i.FechaIngreso) between '2016' and '2023' and -- range of dates just for testing 
 	pp.ParametroDescr like 'fallecimiento' -- only death patients at the moment of the inpatient visit 
