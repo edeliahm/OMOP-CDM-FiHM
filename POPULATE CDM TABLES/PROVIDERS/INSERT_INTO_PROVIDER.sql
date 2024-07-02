c857ef982e4f9d5d22b97e9ecd6708bb8424347b
Emiliano DElia Cerchi <144276034+edeliahm@users.noreply.github.com>
2024-07-02 15:23:13 +0200

    Update INSERT_INTO_PROVIDER.sql

--- POPULATE CDM TABLES/PROVIDERS/INSERT_INTO_PROVIDER.sql
+++ POPULATE CDM TABLES/PROVIDERS/INSERT_INTO_PROVIDER.sql
@@ -7,18 +7,18 @@ BEGIN
 END;
 
 INSERT INTO BBDD_EmilianoDelia.OMOP_CDM.PROVIDER(provider_id, 
-											provider_name, 
-											npi, 
-											dea, 
-											specialty_concept_id, 
-											care_site_id, 
-											year_of_birth, 
-											gender_concept_id, 
-											provider_source_value,
-											specialty_source_value, 
-											specialty_source_concept_id, 
-											gender_source_value, 
-											gender_source_concept_id)
+	provider_name, 
+	npi, 
+	dea, 
+	specialty_concept_id, 
+	care_site_id, 
+	year_of_birth, 
+	gender_concept_id, 
+	provider_source_value,
+	specialty_source_value, 
+	specialty_source_concept_id, 
+	gender_source_value, 
+	gender_source_concept_id)
 
 SELECT DISTINCT
 	d.iddoctor as provider_id, 
