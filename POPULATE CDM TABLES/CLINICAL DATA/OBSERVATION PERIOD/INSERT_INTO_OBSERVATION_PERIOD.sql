344d3731c82a790cf3f7313e83e2e19bbf1d13cd
Emiliano DElia Cerchi <144276034+edeliahm@users.noreply.github.com>
2024-07-09 16:54:39 +0200

    Add files via upload

--- POPULATE CDM TABLES/CLINICAL DATA/OBSERVATION PERIOD/INSERT_INTO_OBSERVATION_PERIOD.sql
+++ POPULATE CDM TABLES/CLINICAL DATA/OBSERVATION PERIOD/INSERT_INTO_OBSERVATION_PERIOD.sql
@@ -1,4 +1,6 @@
-TRUNCATE TABLE BBDD_EMILIANODELIA.OMOP_CDM.OBSERVATION_PERIOD;
+SET DATEFORMAT 'YMD'
+
+TRUNCATE TABLE bbdd_emiliano.omop_cdm.observation_period;
 
 USE Hosma;
 
@@ -7,47 +9,49 @@ WITH cte AS (
 SELECT DISTINCT 
 	'' as observation_period_id, 
 	P.idpaciente as person_id, 
-	i.FechaIngreso as observation_period_start_date, 
-	i.FechaAlta as observation_period_end_date, 
+	convert(date, i.FechaIngreso) as observation_period_start_date, 
+	convert(date, i.FechaAlta) as observation_period_end_date, 
 	32817 as period_type_concept_id   -- domain: type concept / vocab: type concept / name: EHR 
 
 FROM ingresos i 
-JOIN Organizacion o ON O.IdHospital = i.idhospital
-JOIN PACIENTES P ON P.IdPaciente = I.IdPaciente
+	JOIN Organizacion o ON O.IdHospital = i.idhospital
+	JOIN PACIENTES P ON P.IdPaciente = I.IdPaciente
 
 WHERE 
-	I.FechaIngreso >= '2016-01-01' AND 
-	i.fechaingreso <= '2023-12-31' AND 
-	I.FECHAALTA IS NOT NULL AND -- observation_end_date cannot be null
-	P.FECHANAC IS NOT NULL
+		CONVERT(DATE, I.FechaIngreso) >= '2016-01-01' 
+	AND CONVERT(DATE, i.FechaAlta) <= GETDATE()  
+	AND I.fechaingreso IS NOT NULL  -- observation_end_date cannot be null
+	AND P.FECHANAC IS NOT NULL
 
 UNION ALL 
 
 SELECT DISTINCT
 	'' as observation_period_id, 
 	p.idpaciente as person_id, 
-	A.FechaIngreso as observation_period_start_date, 
-	A.FechaIngreso as observation_period_end_date, 
+	convert(date, A.FechaIngreso) as observation_period_start_date, 
+	convert(date, A.FechaIngreso) as observation_period_end_date, 
 	32817 as period_type_concept_id -- domain: type concept / vocab; type concept / name: EHR
 
-FROM AMBULANTES A 
-JOIN pacientes p ON p.idPaciente = A.IdPaciente
-JOIN Organizacion o ON O.IdHospital = A.idhospital
-
+FROM ambulantes a
+	JOIN pacientes p ON p.idPaciente = a.IdPaciente
+	JOIN organizacion o ON o.IdHospital = a.idhospital
+   
 WHERE 
-	A.FechaIngreso >= '2016-01-01' AND
-	a.fechaingreso <= '2023-12-31' AND
-	P.FECHANAC IS NOT NULL AND 
-	P.IdPaciente NOT LIKE 1 -- THIS RECORD HOLDS ERRORS 
+		CONVERT(DATE, A.fechaingreso) >= '2016-01-01'
+	AND convert(date, A.fechaingreso) <= GETDATE() 
+	AND fechanac IS NOT NULL  
+	AND P.idpaciente NOT LIKE 1 -- THIS RECORD HOLDS ERRORS 
 )
 
-INSERT INTO BBDD_EmilianoDelia.OMOP_CDM.observation_period ( 
+
+INSERT INTO bbdd_emilianodelia.omop_cdm.observation_period ( 
 	person_id,
 	observation_period_start_date, 
 	observation_period_end_date, 
 	period_type_concept_id)
 
-SELECT
+
+SELECT DISTINCT
 	--'' as observation_period_id, -- field is auto-populated
 	person_id,
 	observation_period_start_date, 
